b6c2f24c41e90273094ab33d1cfddb6b1359c4f4
Purdonkle <a.damn.purdon@gmail.com>
2021-11-13 23:04:10 +0200

    :heavy_check_mark: add firebase google authentication

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,32 @@
+import { auth, signInWithGoogle } from "../lib/firebase";
+
 export default function EnterPage({}) {
+    const user = null;
+    const username = null;
+
     return (
         <main>
-            <h1>Enter Page</h1>
+            {user ? 
+                !username ? <UsernameForm /> : <SignOutButton /> 
+                :
+                <SignInButton />
+            }
         </main>
     )
+}
+
+function SignInButton() {
+    return (
+        <button className='btn-google' onClick={signInWithGoogle}>
+            Sign in with Google
+        </button>
+    );
+}
+
+function SignOutButton() {
+    return <button onClick={() => auth.signOut()}>Sign out</button>
+}
+
+function UsernameForm() {
+
 }
